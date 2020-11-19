7a6a660da114111b24a2c31f48db31940a6f0ce6
minjaesong <alswo9628@gmail.com>
2020-11-19 20:38:55 +0900

    random stuffs

--- assets/emittest.js
+++ assets/emittest.js
@@ -1,7 +1,7 @@
 con.clear();
 con.move(1,1);
-for (let i = 0; i < 2048; i++) {
-    if (i < 1024) con.color_pair(239, 0); else con.color_pair(0, 239);
+for (let i = 0; i < 1024; i++) {
+    if (i < 512) con.color_pair(239, 0); else con.color_pair(0, 239);
 
     let cx = (i%80)+1;
     let cy = ((i/80)|0)+1;
@@ -9,3 +9,5 @@ for (let i = 0; i < 2048; i++) {
     con.move(cy,cx);
     con.addch(i%256);
 }
+
+println();
